11c15de29ad2fa26c4a8a15cfd1e803d91b3cde7
binhbt <binhbt@sigma-solutions.eu>
2018-09-26 14:02:51 +0700

    apply for all activity both toolbar and content

--- binder/src/main/java/com/vn/fa/font/ActivityUtil.java
+++ binder/src/main/java/com/vn/fa/font/ActivityUtil.java
@@ -1,15 +1,15 @@
 package com.vn.fa.font;
 
 import android.app.Activity;
-import android.view.ViewGroup;
+import android.view.View;
 
 public final class ActivityUtil {
     public static void scaleUp(Activity activity, float scale) {
         if (activity != null){
             try {
-                //View root = activity.getWindow().getDecorView().findViewById(android.R.id.content);
-                final ViewGroup root = (ViewGroup) ((ViewGroup) activity
-                        .findViewById(android.R.id.content)).getChildAt(0);
+                View root = activity.getWindow().getDecorView();
+//                final ViewGroup root = (ViewGroup) ((ViewGroup) activity
+//                        .findViewById(android.R.id.content)).getChildAt(0);
                 ViewGroupUtil.scaleUp(root, scale);
             }catch (Exception e){
                 e.printStackTrace();
@@ -20,9 +20,9 @@ public final class ActivityUtil {
     public static void scaleDown(Activity activity, float scale) {
         if (activity != null){
             try {
-                //View root = activity.getWindow().getDecorView().findViewById(android.R.id.content);
-                final ViewGroup root = (ViewGroup) ((ViewGroup) activity
-                        .findViewById(android.R.id.content)).getChildAt(0);
+                View root = activity.getWindow().getDecorView();
+//                final ViewGroup root = (ViewGroup) ((ViewGroup) activity
+//                        .findViewById(android.R.id.content)).getChildAt(0);
                 ViewGroupUtil.scaleDown(root, scale);
             }catch (Exception e){
                 e.printStackTrace();
